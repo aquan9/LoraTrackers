edbf8f09ae85812593e58039a63c9da0351c3354
Andres Quan <aquan9@comcast.net>
2019-05-02 06:20:43 -0600

    Add gpsd output verification, so that we don't store valid values returned from the daemon

--- gps_python.py
+++ gps_python.py
@@ -5,12 +5,22 @@ import time
 
 gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
 
+actual_value = False
+
 while 1:
     time.sleep(1)
     report = gpsd.next()
     latitude = getattr(report, 'lat', 0.0)
     longitude = getattr(report, 'lon', 0.0)
     times = getattr(report, 'time', '')
-    print("latitude: " + str(latitude))
-    print("longitude: " + str(longitude))
-    print("time: " + str(times))
+
+    #Verify that we're getting a real value from gpsd
+    if(latitude == 0.0):
+        actual_value = False
+    else:
+        actual_value = True
+
+    if(actual_value == True):
+        print("latitude: " + str(latitude))
+        print("longitude: " + str(longitude))
+        print("time: " + str(times))
