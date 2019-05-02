6fa4c6c8e61f6e025285da9dea54ccddbe0f3c28
Andres Quan <aquan9@comcast.net>
2019-05-02 06:29:41 -0600

    Change structure of gps script to be library based

--- gps_python.py
+++ gps_python.py
@@ -3,24 +3,26 @@
 from gps import *
 import time
 
-gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+def getGPS():
+    gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
 
-actual_value = False
+    actual_value = False
 
-while 1:
-    time.sleep(1)
-    report = gpsd.next()
-    latitude = getattr(report, 'lat', 0.0)
-    longitude = getattr(report, 'lon', 0.0)
-    times = getattr(report, 'time', '')
+    while 1:
+        time.sleep(1)
+        report = gpsd.next()
+        latitude = getattr(report, 'lat', 0.0)
+        longitude = getattr(report, 'lon', 0.0)
+        times = getattr(report, 'time', '')
 
-    #Verify that we're getting a real value from gpsd
-    if(latitude == 0.0):
-        actual_value = False
-    else:
-        actual_value = True
+        #Verify that we're getting a real value from gpsd
+        if(latitude == 0.0):
+            actual_value = False
+        else:
+            actual_value = True
 
-    if(actual_value == True):
-        print("latitude: " + str(latitude))
-        print("longitude: " + str(longitude))
-        print("time: " + str(times))
+        if(actual_value == True):
+            return("latitude: " + str(latitude) + " longitude: " + str(longitude) + " time: " + str(times))
+
+if __name__ == "__main__":
+    print(getGPS())
