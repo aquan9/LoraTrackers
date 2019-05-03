13959c8df32bb3123f87479fb86b2071d4b7655b
Andres Quan <aquan9@comcast.net>
2019-05-02 18:30:47 -0600

    Create a controller script to control the entire process, and call the other scripts/programs

---
+++ controller.py
@@ -0,0 +1,30 @@
+#A program to control coordinate gathering and transmission
+#Arguments are as follows: python controller.py <ID> <Mode>
+#Mode can be either transmitter or reciever.
+import os
+import sys
+import subprocess
+from gps_python import getGPS
+
+def main():
+    if(len(sys.argv) != 3):
+        print("Incorrect number of arguments")
+        print("Arguments are as follows: python controller.py <ID> <Mode>")
+        return
+
+    node_id = sys.argv[1]
+    node_mode = sys.argv[2]
+    if(node_mode != "sender" and node_mode != "reciever"):
+        print("Mode must be either sender or reciever")
+        return
+
+    if(node_mode == "sender"):
+        while(1):
+            gps_info = getGPS()
+            print(gps_info)
+            subprocess.call(["sudo", "./dragino_lora_app", "sender", gps_info])
+
+    
+
+if __name__ == "__main__":
+    main()
